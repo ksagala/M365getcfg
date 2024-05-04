a4d782571e8c8f042f8821660d2b20414c32ed96
Konrad Sagala <ksagala@outlook.com>
2024-05-04 17:19:36 +0200

    first update

--- get-EXOSettings.ps1
+++ get-EXOSettings.ps1
@@ -1,30 +1,33 @@
 # Connect to Exchange Online
 $credential = Get-Credential
-$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
-Import-PSSession $session -DisableNameChecking
+Connect-ExchangeOnline -Credential $credential
 
 # Export organization settings
-Get-OrganizationConfig | Export-Clixml -Path "C:\Scripts\M365Settings\OrganizationSettings.xml"
+# Get-OrganizationConfig | Export-Clixml -Path "C:\Scripts\M365getcfg\OrganizationSettings.xml"
+Get-OrganizationConfig > "OrganizationSettings.txt"
 
 # Export transport connectors settings
-Get-TransportConfig | Export-Clixml -Path "C:\Scripts\M365Settings\TransportConnectors.xml"
+# Get-TransportConfig | Export-Clixml -Path "C:\Scripts\M365getcfg\TransportConnectors.xml"
+# Get-InboundConnector | Export-Clixml -Path "C:\Scripts\M365getcfg\InboundConnectors.xml"
+# Get-OutboundConnector -IncludeTestModeConnectors | Export-Clixml -Path "C:\Scripts\M365getcfg\OutboundConnectors.xml"
+Get-TransportConfig > "\TransportConnectors.txt"
+Get-InboundConnector > "InboundConnectors.txt"
+Get-OutboundConnector > "OutboundConnectors.txt"
 
 # Export security policies
-Get-HostedContentFilterPolicy | Export-Clixml -Path "C:\Scripts\M365Settings\SecurityPolicies.xml"
+Get-HostedContentFilterPolicy | Export-Clixml -Path "C:\Scripts\M365getcfg\SecurityPolicies.xml"
 
 # Export unified labels
-Get-Label | Export-Clixml -Path "C:\Scripts\M365Settings\UnifiedLabels.xml"
+Get-Label | Export-Clixml -Path "C:\Scripts\M365getcfg\UnifiedLabels.xml"
 
-# Disconnect from Exchange Online
-Remove-PSSession $session
 # Export send and receive connectors settings
-Get-EXOMailboxTransportService | Export-Clixml -Path "C:\Scripts\M365Settings\Connectors.xml"
+Get-EXOMailboxTransportService | Export-Clixml -Path "C:\Scripts\M365getcfg\Connectors.xml"
 
 # Export security policies
-Get-EXOAntiSpamPolicy | Export-Clixml -Path "C:\Scripts\M365Settings\SecurityPolicies.xml"
+Get-EXOAntiSpamPolicy | Export-Clixml -Path "C:\Scripts\M365getcfg\SecurityPolicies.xml"
 
 # Export unified labels
-Get-EXORetentionLabel | Export-Clixml -Path "C:\Scripts\M365Settings\UnifiedLabels.xml"
+Get-EXORetentionLabel | Export-Clixml -Path "C:\Scripts\M365getcfg\UnifiedLabels.xml"
 
 # Disconnect from Exchange Online
 Disconnect-ExchangeOnline
