fc507a72f5de280756603d3ac7b0870c234b4788
Konrad Sagala <ksagala@outlook.com>
2024-05-04 17:51:03 +0200

    second update

--- get-EXOSettings.ps1
+++ get-EXOSettings.ps1
@@ -4,15 +4,15 @@ Connect-ExchangeOnline -Credential $credential
 
 # Export organization settings
 # Get-OrganizationConfig | Export-Clixml -Path "C:\Scripts\M365getcfg\OrganizationSettings.xml"
-Get-OrganizationConfig > "OrganizationSettings.txt"
+Get-OrganizationConfig | Format-List > "OrganizationSettings.txt"
 
 # Export transport connectors settings
 # Get-TransportConfig | Export-Clixml -Path "C:\Scripts\M365getcfg\TransportConnectors.xml"
 # Get-InboundConnector | Export-Clixml -Path "C:\Scripts\M365getcfg\InboundConnectors.xml"
-# Get-OutboundConnector -IncludeTestModeConnectors | Export-Clixml -Path "C:\Scripts\M365getcfg\OutboundConnectors.xml"
-Get-TransportConfig > "\TransportConnectors.txt"
-Get-InboundConnector > "InboundConnectors.txt"
-Get-OutboundConnector > "OutboundConnectors.txt"
+# Get-OutboundConnector -IncludeTestModeConnectors $true | Export-Clixml -Path "C:\Scripts\M365getcfg\OutboundConnectors.xml"
+Get-TransportConfig | Format-List > "TransportConnectors.txt"
+Get-InboundConnector | Format-List > "InboundConnectors.txt"
+Get-OutboundConnector -IncludeTestModeConnectors $true | Format-List > "OutboundConnectors.txt"
 
 # Export security policies
 Get-HostedContentFilterPolicy | Export-Clixml -Path "C:\Scripts\M365getcfg\SecurityPolicies.xml"
